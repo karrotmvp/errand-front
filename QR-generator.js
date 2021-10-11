02635c2f35bc67a3ae31a86022869267c4f84b7a
dd <dd@ddui-MacBookPro.local>
2021-10-11 22:10:03 +0900

    feat: add QR-generator script

---
+++ QR-generator.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { exec } = require("child_process");
+
+const PORT = process.env.PORT || 3000;
+const APP_ID = process.env.APP_ID || 0;
+
+async function generateQR() {
+  try {
+    exec(
+      `karrot-mini dev -p ${PORT} --appId ${APP_ID}`,
+      (error, stdout, stderr) => {
+        console.log(stdout);
+      }
+    );
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+generateQR();
