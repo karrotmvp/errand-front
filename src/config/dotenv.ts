ba7c411365954e4157668ad38665d6f881d851c6
jjunyjjuny <jjunyjjuny@gmail.com>
2021-10-12 23:37:10 +0900

    chore: 각종 유틸 추가

--- src/config/dotenv.ts
+++ src/config/dotenv.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const envs = {
+  BASE_URL: process.env.REACT_APP_BASE_URL || "",
   PRESET: process.env.REACT_APP_PRESET || "",
   APP_ID: process.env.REACT_APP_ID || "",
 };
---
+++ src/utils/axios.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+import envs from "../config/dotenv";
+
+const fetchWrap = async ({
+  method,
+  url,
+  params,
+  body,
+}: {
+  method: "get" | "post" | "patch" | "delete";
+  url: string;
+  params?: {};
+  body?: {};
+}): Promise<any> => {
+  try {
+    const config = {
+      // baseURL: envs.BASE_URL,
+      withCredentials: true,
+      params,
+    };
+    const { data } =
+      (method === "get" && (await axios.get(url, config))) ||
+      (method === "post" && (await axios.post(url, body, config))) ||
+      (method === "patch" && (await axios.patch(url, body, config))) ||
+      (method === "delete" && (await axios.delete(url, config))) ||
+      {};
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const GET = (url: string, params?: {}) =>
+  fetchWrap({ method: "get", url, params });
+
+export const POST = (url: string, body?: {}, params?: {}) =>
+  fetchWrap({ method: "post", url, body });
+
+export const PATCH = (url: string, body?: {}) =>
+  fetchWrap({ method: "patch", url, body });
+
+export const DELETE = (url: string) => fetchWrap({ method: "delete", url });
