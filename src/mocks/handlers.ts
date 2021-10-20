f756e2ca39153f3843b3585207599f4173ae7061
jjunyjjuny <jjuny546@gmail.com>
2021-10-20 17:36:53 +0900

    fix: response 수정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -10,12 +10,11 @@ export const handlers = [
   rest.get("/errands", (req, res, ctx) => {
     const lastId = req.url.searchParams.get("lastId");
     const size = req.url.searchParams.get("size");
-
-    return res(ctx.json({ ...DEFAULT_RESPONSE, data: errandList }));
+    return res(ctx.status(200), ctx.json(errandList));
   }),
 
   rest.post("/errands", (req, res, ctx) => {
-    return res(ctx.json({ ...DEFAULT_RESPONSE, data: { id: 1 } }));
+    return res(ctx.status(200), ctx.json({ id: 1 }));
   }),
 
   rest.get("/errands/:errandId", (req, res, ctx) => {
@@ -43,21 +42,21 @@ export const handlers = [
     my.didIApply = true;
     my.wasIChosen = true;
 
-    return res(ctx.json({ ...DEFAULT_RESPONSE, data: my }));
+    return res(ctx.status(200), ctx.json({ my }));
   }),
 
   rest.get("/errands/:errandId/helpers", (req, res, ctx) => {
-    return res(ctx.json({ ...DEFAULT_RESPONSE, data: applyList }));
+    return res(ctx.status(200), ctx.json({ applyList }));
   }),
 
   rest.get("/errands/:errandId/helpers/:helperId", (req, res, ctx) => {
-    return res(ctx.json({ ...DEFAULT_RESPONSE, data: resume }));
+    return res(ctx.status(200), ctx.json({ resume }));
   }),
 
   rest.patch("/errands/:id/helper", (req, res, ctx) => {
     return res(
+      ctx.status(200),
       ctx.json({
-        ...DEFAULT_RESPONSE,
         message: "성공",
       })
     );
@@ -65,8 +64,8 @@ export const handlers = [
 
   rest.patch("/errands/:id/complete", (req, res, ctx) => {
     return res(
+      ctx.status(200),
       ctx.json({
-        ...DEFAULT_RESPONSE,
         message: "성공",
       })
     );
@@ -82,26 +81,26 @@ export const handlers = [
       helperCtn: 5,
     };
     return res(
+      ctx.status(200),
       ctx.json({
-        ...DEFAULT_RESPONSE,
-        data: can,
+        can,
       })
     );
   }),
   // my
   rest.get("/my/errands", (req, res, ctx) => {
-    return res(ctx.json({ ...DEFAULT_RESPONSE, data: {} }));
+    return res(ctx.status(200), ctx.json({}));
   }),
 
   rest.get("/my/help", (req, res, ctx) => {
-    return res(ctx.json({ ...DEFAULT_RESPONSE, data: {} }));
+    return res(ctx.status(200), ctx.json({}));
   }),
 
   // help
   rest.post("/help", (req, res, ctx) => {
     return res(
+      ctx.status(200),
       ctx.json({
-        ...DEFAULT_RESPONSE,
         message: "성공",
       })
     );
@@ -109,19 +108,17 @@ export const handlers = [
 
   //users
   rest.get("/users/:id", (req, res, ctx) => {
-    return res(ctx.json({ ...DEFAULT_RESPONSE, data: user }));
+    return res(ctx.status(200), ctx.json({ user }));
   }),
 
   rest.patch("/users/category", (req, res, ctx) => {
-    return res(ctx.json({ ...DEFAULT_RESPONSE, data: user }));
+    return res(ctx.status(200), ctx.json({ user }));
   }),
 
   rest.get("/users/my", (req, res, ctx) => {
     return res(
-      ctx.json({
-        ...DEFAULT_RESPONSE,
-        data: { ...user, daangnId: "8a190fa9bb5d4d89b3944dc8c5b3a102" },
-      })
+      ctx.status(200),
+      ctx.json({ ...user, daangnId: "8a190fa9bb5d4d89b3944dc8c5b3a102" })
     );
   }),
 
@@ -130,19 +127,17 @@ export const handlers = [
     const regionId = req.url.searchParams.get("regionId");
 
     return res(
+      ctx.status(200),
       ctx.json({
-        ...DEFAULT_RESPONSE,
-        data: {
-          name: "역삼1동",
-          name1: "서울특별시",
-          name2: "강남구",
-          name3: "역삼1동",
-        },
+        name: "역삼1동",
+        name1: "서울특별시",
+        name2: "강남구",
+        name3: "역삼1동",
       })
     );
   }),
 
   rest.post("/login", (req, res, ctx) => {
-    return res(ctx.json({ ...DEFAULT_RESPONSE, data: {} }));
+    return res(ctx.status(200), ctx.json({}));
   }),
 ];
