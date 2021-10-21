15dd053f67c76813b059ed7a0f2ad9100389e48c
jjunyjjuny <jjuny546@gmail.com>
2021-10-21 15:36:39 +0900

    chore: svg 추가

---
+++ src/assets/icon/index.ts
@@ -0,0 +1,4 @@
+export { default as Gear } from "./gear.svg";
+export { default as Back } from "./back_arrow.svg";
+export { default as Me } from "./me.svg";
+export { default as Plus } from "./me.svg";
