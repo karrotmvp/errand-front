84ff5309ac188143002e58cfac91574c0fc6fb07
jjunyjjuny <jjuny546@gmail.com>
2021-10-29 00:01:19 +0900

    chore: home에서 tooltip 사용

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,73 +1,105 @@
-import { Gear, Map, Me } from "@assets/icon";
+import { Gear, Map, Me, Plus } from "@assets/icon";
 import CustomScreenHelmet from "@components/CustomScreenHelmet";
+import ToolTip from "@components/ToolTip";
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import usePush from "@hooks/usePush";
+import { useState } from "react";
 import List from "./List";
 
 export default function Home() {
   const moveToApplyForm = usePush("/errand-request");
-
+  const [showTooltip, setShowTooltip] = useState<boolean>(true);
+  const closeTooltip = () => {
+    setShowTooltip(false);
+  };
   return (
     <>
-      <CustomScreenHelmet title="서현동" appendRight={RightAppender()} />
+      <CustomScreenHelmet
+        title={
+          <Title>
+            <h1>당근심부름</h1>
+            <span>Beta</span>
+          </Title>
+        }
+        appendRight={RightAppender()}
+      />
       <HomeWrapper>
         <ContentWrapper>
-          <div className="home-banner">
+          <div className="home__banner">
             <Map />
             <span>서현동</span>의 심부름
           </div>
+          {showTooltip && (
+            <div className="home__container">
+              <ToolTip
+                text="당근마켓에서 인증한 동네에서 심부름을 요청할 수 있어요."
+                closeTooltip={closeTooltip}
+              />
+            </div>
+          )}
+
           <List tabType="main" />
         </ContentWrapper>
-        <button className="home-fixed__fab" onClick={moveToApplyForm}>
-          +
+        <button className="home__fixed-fab" onClick={moveToApplyForm}>
+          <Plus />
         </button>
       </HomeWrapper>
     </>
   );
 }
 
+const Title = styled.div`
+  display: flex;
+  align-items: flex-end;
+
+  & > span {
+    color: ${({ theme }) => theme.color.primary};
+    font-size: 1.2rem;
+    font-weight: 500;
+    margin-left: 0.5rem;
+  }
+`;
+
 const HomeWrapper = styled.main`
   height: 100%;
   position: relative;
-  .home-banner {
-    ${({ theme }) => theme.container}
-    margin-top: 2rem;
-    font-size: 1.7rem;
-    font-weight: 700;
+  .home {
+    &__banner {
+      ${({ theme }) => theme.container}
+      margin-top: 2rem;
+      font-size: 1.7rem;
+      font-weight: 700;
 
-    display: flex;
-    align-items: center;
+      display: flex;
+      align-items: center;
 
-    span {
-      color: ${({ theme }) => theme.color.primary};
+      span {
+        color: ${({ theme }) => theme.color.primary};
+      }
     }
-  }
+    &__container {
+      ${({ theme }) => theme.container}
+    }
+    &__fixed-fab {
+      position: absolute;
+      bottom: 2rem;
+      right: 2rem;
 
-  .home-fixed__fab {
-    position: absolute;
-    bottom: 2rem;
-    right: 2rem;
+      width: 5.7rem;
+      height: 5.7rem;
+      background: ${({ theme }) => theme.color.primary};
+      border-radius: 3rem;
 
-    width: 5.7rem;
-    height: 5.7rem;
-    background: ${({ theme }) => theme.color.primary};
-    border-radius: 3rem;
-
-    color: white;
-    // TODO. SVG 아이콘으로 대체
-    font-size: 23px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
   }
 `;
 const ContentWrapper = styled.div`
   height: 100%;
   overflow-y: scroll;
-  p {
-    ${({ theme }) => css`
-      ${theme.container}
-      ${theme.font("large", "bold")}
-    `}
-  }
 `;
 
 const RightAppender = () => {
