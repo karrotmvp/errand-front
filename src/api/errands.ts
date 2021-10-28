3259ad242c072bbe19dcd2a35f654e52ae6bbf76
jjunyjjuny <jjuny546@gmail.com>
2021-10-28 13:45:52 +0900

    feat: 무한스크롤 관련 api

--- src/api/errands.ts
+++ src/api/errands.ts
@@ -1,30 +1,41 @@
 import { useQuery, useInfiniteQuery } from "react-query";
 import { GET, PATCH, POST } from "@utils/axios";
 import { Errand, ErrandDetailResponseBody, Resume, User } from "@type/response";
-import { ErrandRequestParams } from "@type/client";
+import { TabType } from "@type/client";
 import {
   ErrandRegisterRequestBody,
   SelecteHelperRequestBody,
 } from "@type/request";
 import { ERREND_REQUEST_SIZE } from "@constant/request";
 
-const getErrandList = (lastId = 0): Promise<Errand[]> => {
-  return GET(`/errands?lastId=${lastId}&size=${ERREND_REQUEST_SIZE}`);
+const getMainErrands = ({ pageParam = 0 }): Promise<Errand[]> => {
+  return GET(`/errands?lastId=${pageParam}&size=${ERREND_REQUEST_SIZE}`);
+};
+const getMyErrands = ({ pageParam = 0 }): Promise<Errand[]> => {
+  return GET(`my/errands?lastId=${pageParam}&size=${ERREND_REQUEST_SIZE}`);
+};
+const getMyHelps = ({ pageParam = 0 }): Promise<Errand[]> => {
+  return GET(`my/helps?lastId=${pageParam}&size=${ERREND_REQUEST_SIZE}`);
 };
 
-export const useErrandList = ({ lastId }: ErrandRequestParams) => {
-  return useInfiniteQuery(
-    ["errandList", lastId],
-    ({ pageParam = 0 }): Promise<Errand[]> => {
-      return GET(`/errands?lastId=${lastId}&size=${ERREND_REQUEST_SIZE}`);
+const fetchWrap = (tabType: TabType) => {
+  switch (tabType) {
+    case "main":
+      return getMainErrands;
+    case "request":
+      return getMyErrands;
+    case "help":
+      return getMyHelps;
+  }
+};
+
+export const useErrandList = (tabType: TabType) => {
+  return useInfiniteQuery([tabType], fetchWrap(tabType), {
+    getNextPageParam: (lastErrans: Errand[]) => {
+      const lastErrand = lastErrans[lastErrans.length - 1];
+      return lastErrand.id ?? false;
     },
-    {
-      getNextPageParam: (lastErrans: Errand[]) => {
-        const lastErrand = lastErrans[lastErrans.length - 1];
-        return lastErrand.id ?? false;
-      },
-    }
-  );
+  });
 };
 
 export const registerErrand = (requestBody: ErrandRegisterRequestBody) => {
