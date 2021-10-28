a5007ecf948a8cbe10d05b9f195a4d5006c58d17
jjunyjjuny <jjuny546@gmail.com>
2021-10-28 23:43:26 +0900

    feat: toogleSwitch 컴포넌트

--- src/components/ToggleSwitch/index.tsx
+++ src/components/ToggleSwitch/index.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import styled from "@emotion/styled";
+import { css } from "@emotion/css";
 
 type ToggleSwitchProps = {
   callback: (result: boolean) => void;
   defaultValue?: boolean;
+  height?: number;
 };
 
 export default function ToggleSwitch({
   callback,
   defaultValue = false,
+  height = 2.8,
 }: ToggleSwitchProps) {
   const [toggle, setToggle] = useState(defaultValue);
   const handleToggle = () => {
@@ -20,12 +23,27 @@ export default function ToggleSwitch({
   }, [toggle, callback]);
 
   return (
-    <ToggleSwitchWrapper onClick={handleToggle}>
-      <div>{toggle ? "온" : "오푸"}</div>
+    <ToggleSwitchWrapper onClick={handleToggle} height={height} toggle={toggle}>
+      <div className="toggle__circle" />
     </ToggleSwitchWrapper>
   );
 }
 
-const ToggleSwitchWrapper = styled.div`
-  display: flex;
+const ToggleSwitchWrapper = styled.div<{ height: number; toggle: boolean }>`
+  background: ${({ theme }) => theme.color.grey7};
+
+  width: ${({ height }) => `${2 * height}rem`};
+  border-radius: ${({ height }) => `${height}rem`};
+
+  padding: 0.2rem;
+
+  .toggle__circle {
+    background: ${({ toggle, theme }) =>
+      toggle ? theme.color.primary : theme.color.grey4};
+    width: ${({ height }) => `${height}rem`};
+    height: ${({ height }) => `${height}rem`};
+    border-radius: ${({ height }) => `${height}rem`};
+    transition: all 0.3s;
+    transform: translateX(${({ toggle }) => (toggle ? `2.4rem` : "0")});
+  }
 `;
