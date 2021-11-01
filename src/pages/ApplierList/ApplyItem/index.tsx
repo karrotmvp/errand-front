323f5caa432e2d513750ec52c0af3c7f5d9e9dcf
jjunyjjuny <jjuny546@gmail.com>
2021-11-01 23:50:32 +0900

    feat: 지원자 목록 페이지 마크업

--- src/pages/ApplierList/ApplyItem/index.tsx
+++ src/pages/ApplierList/ApplyItem/index.tsx
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 import { User } from "@type/response";
 import Profile from "@components/Profile";
 import usePush from "@hooks/usePush";
+import { Right } from "@assets/icon";
 
 type ApplyItemProps = {
   helper: User;
@@ -11,11 +12,15 @@ type ApplyItemProps = {
 export default function ApplyItem({ helper }: ApplyItemProps) {
   const { id, nickname, regionName, mannerPoint, profileImgUrl } = helper;
   const moveToUserDetail = usePush(`/appliers/${id}`);
-
+  const appeal =
+    "저 강아지를 진짜 좋아해요 ㅎㅎ 지금 집에서 한마리 키우고 있는데 같이 산책하면 좋을 것";
   return (
     <ApplyItemWrapper onClick={moveToUserDetail}>
-      <Profile {...{ nickname, regionName, mannerPoint, profileImgUrl }} />
-      {">"}
+      <div className="apply-item__profile">
+        <Profile {...{ nickname, regionName, mannerPoint, profileImgUrl }} />
+        <Right />
+      </div>
+      <div className="apply-item__appeal">{appeal}</div>
     </ApplyItemWrapper>
   );
 }
@@ -23,9 +28,23 @@ export default function ApplyItem({ helper }: ApplyItemProps) {
 const ApplyItemWrapper = styled.div`
   padding: 2.5rem;
   ${({ theme }) => theme.container}
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+
+  .apply-item {
+    &__profile {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+    &__appeal {
+      ${({ theme }) => theme.font("medium", "regular")}
+      margin-top: 2.3rem;
+      border-radius: 0.8rem;
+      background: ${({ theme }) => theme.color.grey8};
+      padding: 2rem 3.2rem;
+    }
+  }
+
   & + & {
     margin-top: 0.1rem solid black;
   }
