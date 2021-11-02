1764893278840d1264adf75c9121c14bb22cc8a3
jjunyjjuny <jjuny546@gmail.com>
2021-11-02 22:51:09 +0900

    chore: api path users => user

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "react-query";
 import { GET } from "@utils/axios";
 import { User } from "@type/response";
+import { getValueFromSearch } from "@utils/utils";
 
 const getUserProfile = async (id: number): Promise<User> => {
-  const { data } = await GET(`/users/:${id}`);
+  const { data } = await GET(`/user/:${id}`);
   return data;
 };
 export const useUserProfile = (id: number) => {
@@ -11,9 +12,19 @@ export const useUserProfile = (id: number) => {
 };
 
 const getMyInfo = async (): Promise<User> => {
-  const { data } = await GET(`/users/my`);
+  const regionId = getValueFromSearch("region_id");
+  const { data } = await GET(`/user/my?regionId=${regionId}`);
   return data;
 };
 export const useMyInfo = () => {
   return useQuery(["myInfo"], () => getMyInfo());
+};
+
+const getUserAlarm = async () => {
+  const { data } = await GET(`user/alarm`);
+  return data;
+};
+
+export const useUserAlarm = () => {
+  return useQuery(["userAlarm"], () => getUserAlarm());
 };
