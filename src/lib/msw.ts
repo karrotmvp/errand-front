f4ca6eb7ca9b618d416b460a47a44031b7d3f430
jjunyjjuny <jjuny546@gmail.com>
2021-11-05 21:10:53 +0900

    chore: 프론트 서버 디버깅 console

--- src/lib/msw.ts
+++ src/lib/msw.ts
@@ -1,4 +1,5 @@
 export const initMSW = () => {
+  console.log("initMSW : ", process.env.NODE_ENV);
   if (process.env.NODE_ENV === "development") {
     if (typeof window !== "undefined") {
       const { worker } = require("../mocks/browser");
