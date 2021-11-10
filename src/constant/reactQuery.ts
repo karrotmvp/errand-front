7dced2dfcb884ba7013254731e1bfa6f45d82b07
jjunyjjuny <jjuny546@gmail.com>
2021-11-10 11:36:53 +0900

    mutation 인자 타입, react-query KEY 상수화

---
+++ src/constant/reactQuery.ts
@@ -0,0 +1,10 @@
+export const KEYS = {
+  ERRNDS: "getErrands",
+  ERRND_DETAIL: "errandDetail",
+  APPLIERS: "appliers",
+  REGION_INFO: "regionInfo",
+  RESUME: "resume",
+  USER_PROFILE: "userProfile",
+  MY_INFO: "my-info",
+  USER_ALRAMS: "user-alarms",
+};
